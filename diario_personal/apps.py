11ede33acb4949e787f64ba8186474866daadaca
lorena <lfabiola21@gmail.com>
2026-09-02 16:45:01 -0400

    pagina inicio

--- diario_personal/apps.py
+++ diario_personal/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class DiarioPersonalConfig(AppConfig):
+class diariopersonalConfig(AppConfig):
     name = 'diario_personal'
--- diario_personal/views.py
+++ diario_personal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def inicio(request):
-    return render(request, 'diario_personal/inicio.html')
+    return render(request,'diario_personal/inicio.html')
