06e8eeb6d26b0e7ffe33ae344c268b7a1cef9b25
lorena <lfabiola21@gmail.com>
2026-09-04 09:56:50 -0400

    pagina de inicio y error 404

--- django_core/settings.py
+++ django_core/settings.py
@@ -24,10 +24,10 @@ SECRET_KEY = 'django-insecure-%uvmr$mcx9g$du(68kxv84%1ez$@(9ynsty(k(xyb)#)*awl(7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
