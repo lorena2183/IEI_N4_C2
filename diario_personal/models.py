4d7dcc01b3df1086dd9816740291923dbc68206a
lorena <lfabiola21@gmail.com>
2026-09-24 21:54:38 -0300

    modificacion modelo

--- diario_personal/models.py
+++ diario_personal/models.py
@@ -1,34 +1,39 @@
 
 from django.db import models
 
+str_fecha_creacion = "Fecha Creación"
+str_fecha_actualizacion = "Fecha Actualización"
+str_usuario = "Usuario"
+str_entrada ="Entrada"
+str_categoria = "Categoria"
+
 
 class Usuario(models.Model):
-    nombre_usuario = models.CharField(max_length=25)
-    email = models.EmailField()
-    contrasena = models.CharField(max_length=100)
+    nombre_usuario = models.CharField("Usuario",max_length=25,null=False)
+    email = models.EmailField(null=True)
+    contrasena = models.CharField(max_length=100,null=False)
     fecha_nacimiento = models.DateField()
-    es_publico = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(str_fecha_creacion,auto_now_add=True)
+    updated_at = models.DateTimeField(str_fecha_actualizacion,auto_now=True)
 
     def __str__(self):
         return self.nombre_usuario
 
 
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=25)
-    descripcion = models.TextField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    nombre = models.CharField("Categoria",max_length=25,null=False)
+    descripcion = models.TextField(max_length=100,null=False)
+    created_at = models.DateTimeField(str_fecha_creacion,auto_now_add=True)
+    updated_at = models.DateTimeField(str_fecha_actualizacion,auto_now=True)
 
     def __str__(self):
         return self.nombre
 
 
 class Etiqueta(models.Model):
-    nombre = models.CharField(max_length=25)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    nombre = models.CharField("Etiqueta",max_length=25,null=False)
+    created_at = models.DateTimeField(str_fecha_creacion,auto_now_add=True)
+    updated_at = models.DateTimeField(str_fecha_actualizacion,auto_now=True)
 
     def __str__(self):
         return self.nombre
@@ -43,13 +48,9 @@ class EntradaDiario(models.Model):
     fecha = models.DateField()
     hora = models.TimeField()
     estado_animo = models.CharField(max_length=25)
-    es_publica = models.BooleanField(default=False)
     nivel_importancia = models.IntegerField()
-    calificacion = models.DecimalField(max_digits=3, decimal_places=1)
-    latitud = models.FloatField(null=True, blank=True)
-    enlace_referencia = models.URLField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(str_fecha_creacion,auto_now_add=True)
+    updated_at = models.DateTimeField(str_fecha_actualizacion,auto_now=True)
 
     def __str__(self):
         return f"{self.titulo} ({self.fecha})"
